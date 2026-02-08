9ca1fac2b349e1f08c3bd1d1bb7d9b8f2f420def
RohanChittora <rohanchittora876@gmail.com>
2026-02-08 22:59:59 +0530

    Add solution: longest-substring-without-repeating-characters.cpp

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -1,1 +1,25 @@
-"bca"
+class Solution {
+public:
+   int lengthOfLongestSubstring(string s) {
+       int first=0;
+       int second=0;
+       int len=0;
+       vector<bool>temp(256,0);
+       while(second<s.size())
+       {
+           while(temp[s[second]])
+           {
+               temp[s[first]]=0;
+               first++;
+           }
+           temp[s[second]]=1;
+           len=max(len,second-first+1);
+           second++;
+       }
+       return len;
+       
+
+       
+       
+   }
+};
