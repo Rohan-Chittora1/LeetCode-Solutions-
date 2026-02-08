56a0d361268fc20a2835250484b2f84a1a5f3afa
RohanChittora <rohanchittora876@gmail.com>
2026-02-09 00:56:20 +0530

    Add solution: zigzag-conversion.cpp

--- zigzag-conversion.cpp
+++ zigzag-conversion.cpp
@@ -1,3 +1,29 @@
-|    /|    /|    /|
-|  /  |  /  |  /  |
-|/    |/    |/    |
+class Solution {
+public:
+   string convert(string s, int numRows) {
+       int size = s.size();
+       if (numRows == 1 || numRows >= size) {
+           return s;
+       }
+       vector<vector<char>> rows(numRows);
+       int idx = 0;
+       int d = 1;
+       for (char c : s) {
+           rows[idx].push_back(c);
+           if (idx == 0) {
+               d = 1;
+           } else if (idx == numRows - 1) {
+               d = -1;
+           }
+           idx += d;
+       }
+       string result;
+       for (const auto& row : rows) {
+           for (char c : row) {
+               result += c;
+           }
+       }
+
+       return result;
+   }
+};
