509232ffda264de6bc70a20bba6aa4f15c140d19
RohanChittora <rohanchittora876@gmail.com>
2026-02-08 23:11:41 +0530

    Add solution: add-two-numbers.cpp

--- add-two-numbers.cpp
+++ add-two-numbers.cpp
@@ -1,4 +1,59 @@
-¹ 4  2
- 6  5
----------
- 0  7
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+class Solution {
+public:
+   
+   ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+       ListNode*curr1=l1;
+       ListNode*curr2=l2;
+       ListNode*head=new ListNode(0);
+       ListNode*tail=head;
+       int sum=0;
+   
+       int carry=0;
+       while(curr1 && curr2)
+       {
+            sum=curr1->val+curr2->val+carry;
+           carry=sum/10;
+           tail->next=new ListNode(sum%10);
+           curr1=curr1->next;
+           curr2=curr2->next;
+           tail=tail->next;
+       }
+       while(curr1)
+       {
+           sum=curr1->val+carry;
+           carry=sum/10;
+           tail->next=new ListNode(sum%10);
+           curr1=curr1->next;
+           tail=tail->next;
+
+       }
+       while(curr2)
+       {
+           sum=curr2->val+carry;
+           carry=sum/10;
+           tail->next=new ListNode(sum%10);
+           curr2=curr2->next;
+           tail=tail->next;
+
+       }
+       if(carry)
+       {
+           tail->next=new ListNode(carry);
+       }
+       ListNode*result= head->next;
+       delete head;
+       return result;
+
+       
+   }
+};
