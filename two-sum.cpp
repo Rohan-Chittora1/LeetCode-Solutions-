be435f9cd28a05b188b8da8f7bd1d63917b7f104
RohanChittora <rohanchittora876@gmail.com>
2026-02-09 00:50:24 +0530

    Add solution: two-sum.cpp

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,32 +1,12 @@
-class Solution {
-public:
-   vector<int> twoSum(vector<int>& nums, int target) {
-       int n=nums.size();
-       
-       for(int i=0;i<n;i++)
-       {
-           
-           for(int j=i+1;j<n;j++)
-           {
-               if(nums[i]+nums[j]==target)
-               {
-                  
-                  return {i,j};
-                   // a=i;
-                   // b=j;
-                   // while(i!=j){
-                   // b=j;
-                   // }
-                   //LeetCode-Solutions
-                   // version 
-               
-
-               }
-           }
-       }
-       return {};
-   
-       
-       
-   }
-};
+ for(var i =0;i<nums.length;i++){
+    var item = nums[i];   
+    for(var j=0; j < nums.length;j++){
+        var item2 = nums[j];
+        var sum = item + item2;
+        
+    if(sum === target ){
+      return [i,j];
+      
+    }
+  }
+ }
